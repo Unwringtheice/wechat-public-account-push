728c796500a384c8e8e0b02fa720a9d01a89fd84
Unwringtheice <76279997+Unwringtheice@users.noreply.github.com>
2023-10-07 13:41:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oH5qN6FlgRGczdpB-bZ3dkwCLO_E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'B_Hp1XTOtO6oNZldN2K7iuuJ_7d7bhvA77IxJmRUhYs',
+      useTemplateId: 'CO4T9WFsaxPQJUf9vteU8N-LU2lbwC1Uzku7LNby450',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-10-05' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'CO4T9WFsaxPQJUf9vteU8N-LU2lbwC1Uzku7LNby450',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oH5qN6FlgRGczdpB-bZ3dkwCLO_E',
     }
   ],
 
